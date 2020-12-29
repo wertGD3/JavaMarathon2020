01e4c1f8f4249ebe90fb186009c952b175ec3439
wertGD3 <aleksandr.baryshnikov83@gmail.com>
2020-12-29 18:19:14 +0300

    Add day3

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,18 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner c = new Scanner(System.in);
 
+        while (true) {
+            double a = c.nextDouble();
+            double b = c.nextDouble();
+
+            if (b == 0)
+                break;
+            System.out.println(a / b);
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,21 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        for (int i = 0; i < 5; i++) {
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
+
+            if (b == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+
+            System.out.println(a / b);
+        }
     }
 }
