e26a10e8082ce6f9a05f5ab5228b6e60f511c4d1
wertGD3 <aleksandr.baryshnikov83@gmail.com>
2021-01-02 21:55:55 +0300

    Add day 7

--- src/main/java/day7/Task1.java
+++ src/main/java/day7/Task1.java
@@ -2,6 +2,73 @@ package day7;
 
 public class Task1 {
     public static void main(String[] args) {
+        Airplane air1 = new Airplane("airbass", 2012, 42, 4);
+        Airplane air2 = new Airplane("boing", 2011, 40, 5);
+        air1.getLength();
+        air2.getLength();
+        Airplane.compareAirplanes(air1, air2);
+    }
 
+    private static class Airplane {
+        private String producer;
+        private int year;
+        private int length;
+        private int weight;
+        private int fuel = 0;
+
+        public Airplane(String producer, int year, int length, int weight) {
+            this.producer = producer;
+            this.year = year;
+            this.length = length;
+            this.weight = weight;
+        }
+
+        public void setProducer(String producer) {
+            this.producer = producer;
+        }
+
+        public void setYear(int year) {
+            this.year = year;
+        }
+
+        public void setLength(int length) {
+            this.length = length;
+        }
+
+        public int getLength() {
+            return length;
+        }
+
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+
+        public void setFuel(int fuel) {
+            this.fuel = fuel;
+        }
+
+        public int getFuel() {
+            return fuel;
+        }
+
+        private void info() {
+            System.out.println("Изготовитель: " + producer + " ,год выпуска: " + year + " ,длина: " + length + " ,вес: " + weight + " ,количество топлива в баке: " + getFuel());
+        }
+
+        int fillUp(int n) {
+            fuel = +n;
+            return fuel;
+        }
+
+        public static void compareAirplanes(Airplane a, Airplane b) {
+            if (a.getLength() > b.getLength()) {
+                System.out.println("Air1 длиннее air2");
+            } else if (a.getLength() < b.getLength()) {
+                System.out.println("Air2 длиннее air1");
+            } else {
+                System.out.println("Air1 и air2 одинаковы по длине");
+            }
+        }
     }
 }
+
--- src/main/java/day7/Task2.java
+++ src/main/java/day7/Task2.java
@@ -1,7 +1,46 @@
 package day7;
 
+import java.util.Random;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        Player pl1 = new Player(95);
+        Player pl2 = new Player(98);
+        Player pl3 = new Player(93);
+        Player pl4 = new Player(92);
+        Player pl5 = new Player(91);
+        Player pl6 = new Player(97);
+        Player pl7 = new Player(98);
     }
 }
+
+class Player {
+    private int stamina;
+    private static final int MAX_STAMINA = 100;
+    private static final int MIN_STAMINA = 0;
+    private static int countPlayers;
+
+    public Player(int a) {
+        stamina = a;
+        if (countPlayers < 6) {
+            countPlayers++;
+        }
+    }
+
+    public void run() {
+        if (stamina == 0)
+            return;
+        stamina--;
+        if (stamina == 0) {
+            countPlayers--;
+        }
+    }
+
+    public static void info() {
+        if (countPlayers < 6) {
+            System.out.println("Команды неполные,еще есть " + (6 - countPlayers) + " свободных мест");
+
+        } else System.out.println("Мест в командах больше нет");
+    }
+}
+
