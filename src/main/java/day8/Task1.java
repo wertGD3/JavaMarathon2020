78a440eb587c419f6c3396f703f88d1ee0660ad4
wertGD3 <aleksandr.baryshnikov83@gmail.com>
2021-01-03 16:01:16 +0300

    Add day 8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,30 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        String text = " ";
 
+        long one = System.currentTimeMillis();
+
+        for (int i = 0; i < 20001; i++)
+            text += i + " ";
+
+        long two = System.currentTimeMillis();
+
+        System.out.println("Время выполнения String: " + (two - one));
+        System.out.println(text);
+
+        StringBuilder sb = new StringBuilder(" ");
+
+        long three = System.currentTimeMillis();
+
+        for (int i = 0; i < 20001; i++)
+            sb.append(i).append(" ");
+
+        long four = System.currentTimeMillis();
+
+        System.out.println("Врямя выполнения StringBuilder: " + (four - three));
+        System.out.println(sb.toString());
     }
 }
+
+
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,66 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
+        Airplane air = new Airplane("boing", 2010, 40, 5);
+        air.setYear(2012);
+        air.setLength(42);
+        air.fillUp(300);
+        air.fillUp(350);
+        air.info();
+        System.out.println(air.toString());
+    }
 
+    private static class Airplane {
+        private String producer;
+        private int year;
+        private int length;
+        private int weight;
+        private int fuel = 0;
+
+        public Airplane(String producer, int year, int length, int weight) {
+            this.producer = producer;
+            this.year = year;
+            this.length = length;
+            this.weight = weight;
+        }
+
+        public void setProducer(String producer) {
+            this.producer = producer;
+        }
+
+        public void setYear(int year) {
+            this.year = year;
+        }
+
+        public void setLength(int length) {
+            this.length = length;
+        }
+
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+
+        public void setFuel(int fuel) {
+            this.fuel = fuel;
+        }
+
+        public int getFuel() {
+            return fuel;
+        }
+
+        private void info() {
+            System.out.println("Изготовитель: " + producer + " ,год выпуска: " + year + " ,длина: " + length + " ,вес: " + weight + " ,количество топлива в баке: " + getFuel());
+        }
+
+        int fillUp(int n) {
+            fuel = +n;
+            return fuel;
+        }
+
+        @Override
+        public String toString() {
+            return "Изготовитель: " + producer + " ,год выпуска: " + year + " ,длина: " + length + " ,вес: " + weight + " ,количество топлива в баке: " + getFuel();
+        }
     }
 }
+
