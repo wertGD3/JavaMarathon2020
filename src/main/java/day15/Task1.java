ebf7ea5994ef7fc0cec316d065a1feb97f942933
wertGD3 <aleksandr.baryshnikov83@gmail.com>
2021-01-13 00:51:13 +0300

    Add day15

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,26 @@
 package day15;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file1 = new File("src/main/resources/shoes.csv");
+        Scanner scan = new Scanner(file1);
+
+        File fileWrite = new File("src/main/resources/missing_shoes.txt");
+        PrintWriter pw = new PrintWriter(fileWrite);
+        while (scan.hasNextLine()) {
+            String[] massiv = scan.nextLine().split(";");
+            if (Integer.parseInt(massiv[2]) == 0) {
+                pw.println(massiv[0] + " " + massiv[1] + " " + massiv[2]);
+            }
+        }
+        pw.close();
+        scan.close();
     }
 }
+
