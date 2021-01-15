72558e65eb390854dff71ec7115589085117d67f
wertGD3 <aleksandr.baryshnikov83@gmail.com>
2021-01-15 23:52:25 +0300

    Add day17

--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -2,6 +2,12 @@ package day17;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        ChessPiece[] pieces = {ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE, ChessPiece.ROOK_BLACK,
+                ChessPiece.ROOK_BLACK, ChessPiece.ROOK_BLACK, ChessPiece.ROOK_BLACK
+        };
+        for (ChessPiece piece : pieces)
+            System.out.print(piece.getName() + " ");
     }
+
 }
+
