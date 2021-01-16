2069c3473d48a6bf0d887d958f0f51dae7c400a6
wertGD3 <aleksandr.baryshnikov83@gmail.com>
2021-01-16 11:53:34 +0300

    Add day18

--- src/main/java/day18/Task1.java
+++ src/main/java/day18/Task1.java
@@ -2,6 +2,13 @@ package day18;
 
 public class Task1 {
     public static void main(String[] args) {
+        int[] number = {23, 56, 2345, 89, 65};
+        System.out.println(sum(number, 0));
+    }
 
+    public static int sum(int[] number, int i) {
+        if (i == number.length)
+            return 0;
+        return number[i] + sum(number, i + 1);
     }
 }
--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -2,6 +2,15 @@ package day18;
 
 public class Task2 {
     public static void main(String[] args) {
+        System.out.println(count7(717771237));
 
+    }
+
+    public static int count7(int number) {
+        if (number == 0)
+            return 0;
+        if (number % 10 == 7)
+            return 1 + count7(number / 10);
+        else return count7(number / 10);
     }
 }
